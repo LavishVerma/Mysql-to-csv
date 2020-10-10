ec76469f20a411272592bac7e12fc8bc46bd6b72
unknown <lavishverma97@gmail.com>
2020-10-10 18:38:02 +0530

    Swagger added

--- src/main/java/com/example/DbtoFile/configuration/DatasourceConfig.java
+++ src/main/java/com/example/DbtoFile/configuration/DatasourceConfig.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import javax.sql.DataSource;
 
@@ -24,5 +27,11 @@ public class DatasourceConfig {
                 .username(config.getUsername())
                 .password(config.getPassword())
                 .build();
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.DbtoFile")).build();
     }
 }
